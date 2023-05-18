bfe6fd179afbb37cfb36c473f7e0d5274abc077a
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2023-05-18 13:11:50 -0600

    feat: add filter

--- lib/users_api_web/controllers/user_controller.ex
+++ lib/users_api_web/controllers/user_controller.ex
@@ -7,4 +7,11 @@ defmodule UsersApiWeb.UserController do
   def list(conn, params) do
     json(conn, [%{id: 1, name: "Juan"}, %{id: 2, name: "Jose"}])
   end
+
+  def filter(conn, _params) do
+    IO.puts("It works")
+
+    conn
+    |> json(%{"success" => true})
+  end
 end
